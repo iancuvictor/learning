80cc6ed7c03285d889e60d903ecdd25c68015d81
Iancu Victor-Iuliu <iancuvictorwork@gmail.com>
2026-05-02 09:17:58 +0300

    First JS project. Quiz game, very succint

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
-for(let userNum = +prompt('Enter a number', '');;){
-    if(userNum < 100){
-        alert('try again');
-        userNum = +prompt('Enter a number', '')
-    } else {
-        alert('great'); 
-        break;
-    }
+// creating a quiz game to utilize what i've learned up until now.
+let score = 0;
+
+let questionOne = {
+  question: 'How many years in a decade?',
+  answer: 10
+}
+
+let questionTwo = {
+  question: 'How many minutes in an hour?',
+  answer: 60
+}
+
+let questionThree = {
+  question: 'How many seconds in an hour?',
+  answer: 3600
+}
+
+let questionFour = {
+  question: 'What is 10+10?',
+  answer: 20
+}
+
+let qArray = [questionOne, questionTwo, questionThree, questionFour];
+
+function askQuestion(obj){
+  let answer = prompt(`${obj.question}`, '');
+  if(answer == obj['answer']){
+    score++;
+  } else {
+    alert(`Wrong, but going to the next question anyway.`);
+  }
+}
+
+for(let objects of qArray){
+  askQuestion(objects);
+}
+
+if(score >= 2){
+    alert(`You passed with a score of ${score}`)
+} else {
+  alert(`You failed with a score of ${score}`)
 }
