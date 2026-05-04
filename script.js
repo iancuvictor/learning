71813210ed56330eaaee7b5475a43ffce9b7c96c
Iancu Victor-Iuliu <iancuvictorwork@gmail.com>
2026-05-04 13:47:01 +0300

    push didn't work before, trying again. non important desc

--- script.js
+++ script.js
@@ -1,43 +1,12 @@
-// creating a quiz game to utilize what i've learned up until now.
-let score = 0;
-
-let questionOne = {
-  question: 'How many years in a decade?',
-  answer: 10
-}
-
-let questionTwo = {
-  question: 'How many minutes in an hour?',
-  answer: 60
-}
-
-let questionThree = {
-  question: 'How many seconds in an hour?',
-  answer: 3600
-}
-
-let questionFour = {
-  question: 'What is 10+10?',
-  answer: 20
-}
-
-let qArray = [questionOne, questionTwo, questionThree, questionFour];
-
-function askQuestion(obj){
-  let answer = prompt(`${obj.question}`, '');
-  if(answer == obj['answer']){
-    score++;
+function truncate(str, maxlength){
+  if(str.length > maxlength){
+    let newStr = str.slice(0, maxlength) + '...';
+    alert(newStr);
   } else {
-    alert(`Wrong, but going to the next question anyway.`);
+    alert(str);
   }
 }
 
-for(let objects of qArray){
-  askQuestion(objects);
-}
 
-if(score >= 2){
-    alert(`You passed with a score of ${score}`)
-} else {
-  alert(`You failed with a score of ${score}`)
-}
+truncate("What I'd like to tell on this topic is:", 20);
+truncate("Hi everyone!", 20);
